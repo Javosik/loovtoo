ecc1da0366a257fbad40cff2241956857d6671a9
Siimon Härm <siimonha@ut.ee>
2022-12-08 18:38:09 +0200

    fixed errors

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 var canvas = document.getElementById('viewport'),
 context = canvas.getContext('2d');
-var c_sqr = new Path2D
-var g_sqr = new Path2D
-var hd_sqr = new Path2D
-var r_sqr = new Path2D
-var psu_sqr = new Path2D
-var co_sqr = new Path2D
 var moving = false
 
 class Hitbox {
-  constructor(x, y, height, width) {
+  constructor(x, y, width, height) {
     this.x = x
     this.y = y
-    this.height = height
     this.width = width
+    this.height = height
   }
 
   setBorder() {
     var border = new Path2D
-    border.rect(this.x, this.y, this.height, this.width)
+    border.rect(this.x, this.y, this.width, this.height)
     return border
   }
 }
 
 class Component {
-  constructor(x, y, isInPlace, element) {
+  constructor(x, y, isInPlace, element, flag) {
     this.x = x
     this.y = y
     this.isInPlace = isInPlace
     this.element = element
+    this.flag = flag
   }
 
-  setPlace() {
+  setPlace(x,y) {
+    this.x = x
+    this.y = y
     this.element.style.left = this.x+'px',
     this.element.style.top = this.y+'px'
   }
@@ -48,6 +45,11 @@ class Component {
     this.isInPlace = !this.isInPlace
     return this.isInPlace
   }
+
+  setFlag() {
+    this.flag = !this.flag
+    return this.flag
+  }
 }
 
 make_base();
@@ -61,107 +63,111 @@ function make_base() {
     
     //emaplaadi ruut
     mb_sqr = new Hitbox(30, 70, 500, 580)
-    context.stroke(mb_sqr.setBorder());
+    mb_sqr_path = mb_sqr.setBorder()
+    context.stroke(mb_sqr_path);
 
     //cpu ruut
     c_sqr = new Hitbox(225, 210, 120,120)
-    context.stroke(c_sqr.setBorder());
+    c_sqr_path = c_sqr.setBorder()
+    context.stroke(c_sqr_path);
     
     //gpu ruut
     g_sqr = new Hitbox(5,480,500,70)
-    context.stroke(g_sqr.setBorder())
+    g_sqr_path = g_sqr.setBorder()
+    context.stroke(g_sqr_path)
 
     //ram ruut
     r_sqr = new Hitbox(456,108,70,300)
-    context.stroke(r_sqr.setBorder())
+    r_sqr_path = r_sqr.setBorder()
+    context.stroke(r_sqr_path)
 
     //psu ruut
     psu_sqr = new Hitbox(59,687,350,200)
-    context.stroke(psu_sqr.setBorder())
+    psu_sqr_path = psu_sqr.setBorder()
+    context.stroke(psu_sqr_path)
 
     //hard drive ruut
     hd_sqr = new Hitbox(544,595,300,70)
-    context.stroke(hd_sqr.setBorder())
+    hd_sqr_path = hd_sqr.setBorder()
+    context.stroke(hd_sqr_path)
 
     //cooler ruut
     co_sqr = new Hitbox(185,168,200,200)
-    context.stroke(co_sqr.setBorder())
+    co_sqr_path = co_sqr.setBorder()
+    context.stroke(co_sqr_path)
   }
 
   var motherboard = new Component(996, 19, false, document.getElementById("motherboard"))
-  motherboard.setPlace()
+  motherboard.setPlace(motherboard.x, motherboard.y)
 
   var cpu = new Component(1508, 434, false, document.getElementById("cpu"))
-  cpu.setPlace()
+  cpu.setPlace(cpu.x, cpu.y)
 
   var gpu =  new Component(1503, 605, false, document.getElementById("gpu"))
-  gpu.setPlace()
+  gpu.setPlace(gpu.x, gpu.y)
 
   var hard_drive = new Component(1007, 948, false, document.getElementById("hard-drive"))
-  hard_drive.setPlace()
+  hard_drive.setPlace(hard_drive.x, hard_drive.y)
 
   var ram = new Component(1514, 36, false, document.getElementById("ram"))
-  ram.setPlace()
+  ram.setPlace(ram.x, ram.y)
 
   var psu =new Component(1010, 719, false, document.getElementById("psu"))
-  psu.setPlace()
+  psu.setPlace(psu.x, psu.y)
 
   var cooling =new Component(1414, 719, false, document.getElementById("cooling"))
-  cooling.setPlace()
+  cooling.setPlace(cooling.x, cooling.y)
 
   function move(e) {
 
     var newX = e.clientX-15;
     var newY = e.clientY-15;
-
+    
     image.style.left = newX + "px";
     image.style.top = newY + "px";
 
-    if (context.isPointInPath(mb_sqr, newX, newY) && e.path[0].className == "m1" && mb_sqr_place == false)  {
-      console.log("motherboard is in the right place")
-      image.style.left = 30 + "px";
-      image.style.top = 75 + "px";
-      motherboard.isInPlace = true
-      if (motherboard.isInPlace) {
-        moving = !moving
-      }
-    } else if (context.isPointInPath(c_sqr, newX, newY) && e.path[0].className == "c1")  {
-      console.log("cpu is in the right place")
-      moving = !moving //225, 210
-      image.style.left = 230 + "px";
-      image.style.top = 220 + "px";
-      //document.getElementById("mb1").click;
-    } else if (context.isPointInPath(g_sqr, newX, newY) && e.path[0].className == "g1")  {
-      console.log("gpu is in the right place")
-      moving = !moving //5,480
-      image.style.left = 5 + "px";
-      image.style.top = 480 + "px";
-      //document.getElementById("mb1").click;
-    } else if (context.isPointInPath(r_sqr, newX, newY) && e.path[0].className == "r1")  {
-      console.log("ram is in the right place")
-      moving = !moving //456,108
-      image.style.left = 456 + "px";
-      image.style.top = 108 + "px";
-      //document.getElementById("mb1").click;
-    } else if (context.isPointInPath(psu_sqr, newX, newY) && e.path[0].className == "po1")  {
-      console.log("psu is in the right place")
-      moving = !moving //59,687
-      image.style.left = 59 + "px";
-      image.style.top = 687 + "px";
-      //document.getElementById("mb1").click;
-    } else if (context.isPointInPath(hd_sqr, newX, newY) && e.path[0].className == "hd1")  {
-      console.log("hard drive is in the right place")
-      moving = !moving //544,595
-      image.style.left = 544 + "px";
-      image.style.top = 595 + "px";
-      //document.getElementById("mb1").click;
-    } else if (context.isPointInPath(co_sqr, newX, newY) && e.path[0].className == "co1")  {
-      console.log("cooling is in the right place")
-      moving = !moving //185,168
-      image.style.left = 194 + "px";
-      image.style.top = 175 + "px";
-      //document.getElementById("mb1").click;
+    if (context.isPointInPath(mb_sqr_path, newX, newY) && e.path[0].className == "m1" && !motherboard.isInPlace)  {
+      motherboard.setPlace(30, 75)
+     //motherboard.isInPlace = true
+      return
     } 
+
+    if (context.isPointInPath(c_sqr_path, newX, newY) && e.path[0].className == "c1" && !cpu.isInPlace)  {
+      cpu.setPlace(230, 220)
+      //cpu.isInPlace = true
+      return
+    } 
+
+    if (context.isPointInPath(g_sqr_path, newX, newY) && e.path[0].className == "g1" && !gpu.isInPlace)  {
+      gpu.setPlace(5, 480)
+      //gpu.isInPlace = true
+      return
+    } 
+
+    if (context.isPointInPath(r_sqr_path, newX, newY) && e.path[0].className == "r1" && !ram.isInPlace)  {
+      ram.setPlace(456, 108)
+      //ram.isInPlace = true
+      return
+    } 
+
+    if (context.isPointInPath(psu_sqr_path, newX, newY) && e.path[0].className == "po1" && !psu.isInPlace)  {
+      psu.setPlace(59, 687)
+      //psu.isInPlace = true
+      return
+    }
+
+    if (context.isPointInPath(hd_sqr_path, newX, newY) && e.path[0].className == "hd1" && !hard_drive.isInPlace)  {
+      hard_drive.setPlace(544, 595)
+      //hard_drive.isInPlace = true
+      return
+    } 
+
+    if (context.isPointInPath(co_sqr_path, newX, newY) && e.path[0].className == "co1" && !cooling.isInPlace)  {
+      cooling.setPlace(194, 175)
+      //cooling.isInPlace = true
+      return
+    }
+
   }
 
   function initialClick(e) {
