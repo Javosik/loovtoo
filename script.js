2eec36ea03a22a03458309c5b6c1590b3189970e
Siimon Härm <siimonha@ut.ee>
2022-12-10 14:06:19 +0200

    kõik töötab

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('viewport'),
 context = canvas.getContext('2d');
-var moving = false
+//var moving = false
+var saturated = false
 
 class Hitbox {
   constructor(x, y, width, height) {
@@ -18,12 +19,12 @@ class Hitbox {
 }
 
 class Component {
-  constructor(x, y, isInPlace, element, flag) {
+  constructor(x, y, isInPlace, element, moving) {
     this.x = x
     this.y = y
     this.isInPlace = isInPlace
     this.element = element
-    this.flag = flag
+    this.moving = moving
   }
 
   setPlace(x,y) {
@@ -97,25 +98,25 @@ function make_base() {
     context.stroke(co_sqr_path)
   }
 
-  var motherboard = new Component(996, 19, false, document.getElementById("motherboard"))
+  var motherboard = new Component(996, 19, false, document.getElementById("motherboard"), false)
   motherboard.setPlace(motherboard.x, motherboard.y)
 
-  var cpu = new Component(1508, 434, false, document.getElementById("cpu"))
+  var cpu = new Component(1508, 434, false, document.getElementById("cpu"), false)
   cpu.setPlace(cpu.x, cpu.y)
 
-  var gpu =  new Component(1503, 605, false, document.getElementById("gpu"))
+  var gpu =  new Component(1503, 605, false, document.getElementById("gpu"), false)
   gpu.setPlace(gpu.x, gpu.y)
 
-  var hard_drive = new Component(1007, 948, false, document.getElementById("hard-drive"))
+  var hard_drive = new Component(1007, 948, false, document.getElementById("hard-drive"), false)
   hard_drive.setPlace(hard_drive.x, hard_drive.y)
 
-  var ram = new Component(1514, 36, false, document.getElementById("ram"))
+  var ram = new Component(1514, 36, false, document.getElementById("ram"), false)
   ram.setPlace(ram.x, ram.y)
 
-  var psu =new Component(1010, 719, false, document.getElementById("psu"))
+  var psu =new Component(1010, 719, false, document.getElementById("psu"), false)
   psu.setPlace(psu.x, psu.y)
 
-  var cooling =new Component(1414, 719, false, document.getElementById("cooling"))
+  var cooling =new Component(1414, 719, false, document.getElementById("cooling"), false)
   cooling.setPlace(cooling.x, cooling.y)
 
   function move(e) {
@@ -126,45 +127,45 @@ function make_base() {
     image.style.left = newX + "px";
     image.style.top = newY + "px";
 
-    if (context.isPointInPath(mb_sqr_path, newX, newY) && e.path[0].className == "m1" && !motherboard.isInPlace)  {
+    if (context.isPointInPath(mb_sqr_path, newX, newY) && e.path[0].className == "m1")  {
       motherboard.setPlace(30, 75)
-     //motherboard.isInPlace = true
+      motherboard.isInPlace = true
       return
     } 
 
-    if (context.isPointInPath(c_sqr_path, newX, newY) && e.path[0].className == "c1" && !cpu.isInPlace)  {
+    if (context.isPointInPath(c_sqr_path, newX, newY) && e.path[0].className == "c1")  {
       cpu.setPlace(230, 220)
-      //cpu.isInPlace = true
+      cpu.isInPlace = true
       return
     } 
 
-    if (context.isPointInPath(g_sqr_path, newX, newY) && e.path[0].className == "g1" && !gpu.isInPlace)  {
+    if (context.isPointInPath(g_sqr_path, newX, newY) && e.path[0].className == "g1")  {
       gpu.setPlace(5, 480)
-      //gpu.isInPlace = true
+      gpu.isInPlace = true
       return
     } 
 
-    if (context.isPointInPath(r_sqr_path, newX, newY) && e.path[0].className == "r1" && !ram.isInPlace)  {
+    if (context.isPointInPath(r_sqr_path, newX, newY) && e.path[0].className == "r1")  {
       ram.setPlace(456, 108)
-      //ram.isInPlace = true
+      ram.isInPlace = true
       return
     } 
 
-    if (context.isPointInPath(psu_sqr_path, newX, newY) && e.path[0].className == "po1" && !psu.isInPlace)  {
+    if (context.isPointInPath(psu_sqr_path, newX, newY) && e.path[0].className == "po1")  {
       psu.setPlace(59, 687)
-      //psu.isInPlace = true
+      psu.isInPlace = true
       return
     }
 
-    if (context.isPointInPath(hd_sqr_path, newX, newY) && e.path[0].className == "hd1" && !hard_drive.isInPlace)  {
+    if (context.isPointInPath(hd_sqr_path, newX, newY) && e.path[0].className == "hd1")  {
       hard_drive.setPlace(544, 595)
-      //hard_drive.isInPlace = true
+      hard_drive.isInPlace = true
       return
     } 
 
-    if (context.isPointInPath(co_sqr_path, newX, newY) && e.path[0].className == "co1" && !cooling.isInPlace)  {
+    if (context.isPointInPath(co_sqr_path, newX, newY) && e.path[0].className == "co1")  {
       cooling.setPlace(194, 175)
-      //cooling.isInPlace = true
+      cooling.isInPlace = true
       return
     }
 
@@ -172,17 +173,64 @@ function make_base() {
 
   function initialClick(e) {
 
-    if (moving) {
-      document.removeEventListener("mousemove", move);
-      moving = !moving;
-      return;
-    }
-
-    moving = !moving;
+    var movable = find_element(e)
+    var element = movable.element
+    var img = element.getElementsByTagName('img')[0]
     image = this;
 
-    document.addEventListener("mousemove", move, false);
+    console.log(movable.moving, movable.isInPlace)
 
+    if (movable.moving == true) {
+      if (movable.isInPlace == true) {
+        document.removeEventListener("mousemove", move);
+        img.style.filter = "saturate(0)"
+        saturated = false
+        movable.moving = false
+        return
+      } else {
+        document.removeEventListener("mousemove", move);
+        img.style.filter = "saturate(0)"
+        saturated = false
+        movable.moving = false
+      }
+    } 
+    
+    if (movable.moving == false && movable.isInPlace == false) {
+      document.addEventListener("mousemove", move, false);
+      img.style.filter = "saturate(5)"
+      saturated = true
+      movable.moving = true
+    } else {
+      return
+    }
+  }
+
+  function find_element(e) {
+     switch (e.path[0].className) {
+      case 'm1':
+        return motherboard
+        break;
+      case 'c1':
+        return cpu
+        break;
+      case 'g1':
+        return gpu
+        break;
+      case 'r1':
+        return ram
+        break;
+      case 'po1':
+        return psu
+        break;
+      case 'hd1':
+        return hard_drive
+        break;
+      case 'co1':
+        return cooling
+        break;
+      default:
+        console.log("I don't work")
+     }
   }
 
   window.onload = function () {
